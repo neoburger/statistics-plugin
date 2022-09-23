eebd64ee794a2344fbe937ae5c15a694c676c88b
dusmart <dusmart@qq.com>
2022-09-23 11:04:58 +0800

    Create run-node-self.sh

---
+++ run-node-self.sh
@@ -0,0 +1,15 @@
+cd /app
+mkdir -p data
+mkfifo .tmp
+cat .tmp | (dotnet ../.node/neo-cli/bin/Release/net6.0/neo-cli.dll >log 2>&1 && echo > .tmp)
+
+rm -rf .tmpdir
+mkdir -p .tmpdir/data
+INDEX=`curl https://neoburger.blob.core.windows.net/data/index`
+HEX=`printf "%08X\n" $INDEX`
+SRC=data/${HEX:0:1}/${HEX:1:1}/${HEX:2:1}/${HEX:3:1}/${HEX:4:1}/${HEX:5:1}/${HEX:6:1}/${HEX:7:1}/${INDEX}.json
+while [ -f $SRC ];do echo $SRC && cp $SRC .tmpdir/data/${INDEX}.json && INDEX=$((INDEX+1)); HEX=`printf "%08X\n" $INDEX`;SRC=data/${HEX:0:1}/${HEX:1:1}/${HEX:2:1}/${HEX:3:1}/${HEX:4:1}/${HEX:5:1}/${HEX:6:1}/${HEX:7:1}/${INDEX}.json;done
+echo $INDEX > .tmpdir/index
+
+az storage blob upload-batch --max-connections 200 --overwrite true --destination data --source .tmpdir/data --account-name neoburger --account-key ${TODO} // TODO
+az storage blob upload --overwrite true --container-name data --file .tmpdir/index --account-name neoburger --account-key ${TODO} // TODO
